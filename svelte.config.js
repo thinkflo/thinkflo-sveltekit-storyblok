ff0a3eaacd21f4bd29de14f086778758e4c2d9fc
HalCodes <hal@thinkflo.com>
2023-06-21 13:54:38 +0200

    Update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from "svelte-preprocess";
 import * as dotenv from 'dotenv';
 dotenv.config();
-
-console.log(process.env.ACCESS_TOKEN)
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [
     preprocess({
       postcss: true,
@@ -16,9 +11,6 @@ const config = {
   ], 
 	
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
       '@storyblok/svelte': './node_modules/@storyblok/svelte',
