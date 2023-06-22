5960619bdd9a363e7e55e5de9d8613cd488b992d
Tony Spunei <tony.spunei@yahoo.com>
2023-06-22 13:07:38 +0200

    render Cookie Monster

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { useStoryblokBridge, StoryblokComponent } from '@storyblok/svelte';
+	import Cookies_Monster from '$/Components/UI/Cookies_Monster.svelte';
 
 	export let data;
 	onMount(() => {
@@ -17,4 +18,5 @@
 	{#if data.story}
 		<StoryblokComponent blok={data.story.content} />
 	{/if}
+	<Cookies_Monster />
 </div>
