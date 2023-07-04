d694e8166a96eed52288a609e992ba0409aec0c0
Tony Spunei <tony.spunei@yahoo.com>
2023-07-04 13:19:38 +0200

    fix Page content type console error

--- src/Components/Content_Types/Page.svelte
+++ src/Components/Content_Types/Page.svelte
@@ -1,40 +1,7 @@
 <script>
-	import { onMount, onDestroy } from 'svelte';
 	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
 
 	export let blok;
-
-	let scrollInterval;
-	let scrollRef;
-
-	// Duplicate the first item and add it to the end of your carousel
-	let extendedHero = blok?.Hero?.length > 1 ? [...blok.Hero, blok.Hero[0]] : blok?.Hero;
-
-	const startScrolling = () => {
-		scrollInterval = setInterval(() => {
-			// If we're at the end of the scroll container
-			if (scrollRef.scrollLeft >= scrollRef.scrollWidth - scrollRef.offsetWidth) {
-				// Reset scroll position to the left
-				scrollRef.scrollLeft = 0;
-			} else {
-				// Otherwise, scroll a little to the right
-				scrollRef.scrollBy({ left: 1, behavior: 'smooth' });
-			}
-		}, 4000);
-	};
-
-	const pauseScrolling = () => {
-		clearInterval(scrollInterval);
-	};
-
-	onMount(() => {
-		if (blok?.Hero?.length > 1) return;
-		startScrolling();
-	});
-
-	onDestroy(() => {
-		clearInterval(scrollInterval);
-	});
 </script>
 
 {#key blok}
