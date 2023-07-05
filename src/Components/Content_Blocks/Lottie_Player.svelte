2d32412f61147853df22fe004692fe1b151bd61d
Tony Spunei <tony.spunei@yahoo.com>
2023-07-05 16:35:18 +0200

    task #10 - controll json file parameters from storyblok

--- src/Components/Content_Blocks/Lottie_Player.svelte
+++ src/Components/Content_Blocks/Lottie_Player.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
+	import Constrained_Width from '$/Components/UI/Constrained_Width.svelte';
 
   export let blok;
   let lottiePlayer;
@@ -19,15 +20,15 @@
     if(typeof method !== 'function') return;
 
     const fileChain = blok?.Lottie_Files?.map((file, index) => ({
-      state: 'autoplay',
+      state: file?.State || 'autoplay',
       visibility: index === 0 ? [0, 1.0] : undefined,
-      transition: 'onComplete',
+      transition: file?.Transition || 'onComplete',
       path: file.File.filename,
     }));
-
+    
     animation = method({
       player: lottiePlayer,
-      mode: 'chain',
+      mode: blok?.Mode || 'chain',
       actions: fileChain
     });
     showButton = true;
@@ -42,17 +43,19 @@
 </script>
 
 <section class="bg-gray-200 relative">
-  <lottie-player
-    bind:this={lottiePlayer}
-    src="development-landscape.json"
-    class="mx-auto w-1/2 h-screen"
-    on:load={handleLoad}
-  >
-  </lottie-player>
-  <button on:click={handlePlay} class:hidden={!showButton} class="h-20 w-20 bg-red-500 text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-    Play
-  </button>
-  {#if blok?.Audio_File}
-    <audio bind:this={audioElement} src="{blok?.Audio_File?.filename}" preload="auto"></audio>
-  {/if}
+  <Constrained_Width>
+    <lottie-player
+      bind:this={lottiePlayer}
+      src="development-landscape.json"
+      class="mx-auto w-2/3 h-screen"
+      on:load={handleLoad}
+    >
+    </lottie-player>
+    <button on:click={handlePlay} class:hidden={!showButton} class="h-20 w-20 bg-red-500 text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      Play
+    </button>
+    {#if blok?.Audio_File}
+      <audio bind:this={audioElement} src="{blok?.Audio_File?.filename}" preload="auto"></audio>
+    {/if}
+  </Constrained_Width>
 </section>
