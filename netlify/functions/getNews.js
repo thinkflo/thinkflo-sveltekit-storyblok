f87aa602bdc955f64529716230aa1233aa718d27
HalCodes <hal@thinkflo.com>
2023-07-12 15:26:26 +0200

    Switch to Axios and add topics

--- netlify/functions/getNews.js
+++ netlify/functions/getNews.js
@@ -1,21 +1,27 @@
 import { XMLParser } from "fast-xml-parser";
-import fetch from 'node-fetch';
+import axios from 'axios';
 
-exports.handler = async function (event, context, callback) {
-  const response = await fetch('https://hnrss.org/newest?q=jamstack').then(res => res.text()).catch((error) => {
-    callback(error);
-  })
+export async function handler(event, context, callback) {
+  const topics = ['jamstack', 'ssg', 'ssr', 'static site', 'serverless'];
 
-  const news = new XMLParser().parse(response, {
-    ignoreAttributes: false,
-    attributeNamePrefix: '',
-  });
-
-  const newsArticles = news?.rss?.channel?.item?.filter(item => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')) 
+  const newsArticles = await Promise.allSettled(
+    topics.map(
+      async topic => await axios.get(`https://hnrss.org/newest?q=${topic}`)
+      .then(res => res.data)
+      .catch(
+        (error) => {
+          callback(error);
+        })
+      )
+  ).then((values) => values.map(topic => new XMLParser().parse(topic.value, {
+      ignoreAttributes: false,
+      attributeNamePrefix: '',
+    }).rss?.channel?.item?.filter(item => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')) 
     .map(item => { 
-      return { title : item?.title, url : item?.link }
-    });
-
+      return { title : item?.title, url : item?.link, postDate: item?.pubDate }
+    })
+  ).flat().sort((a, b) => new Date(a.postDate) + new Date(b.postDate)))
+      
   return {
     statusCode: newsArticles?.length ? 200 : 500,
     body: newsArticles?.length ? JSON.stringify({ newsArticles }) : undefined
