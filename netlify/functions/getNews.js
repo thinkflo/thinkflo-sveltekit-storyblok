92f103da4e4911abff7fea8446a0e47cb19d7f5c
HalCodes <hal@thinkflo.com>
2023-07-12 17:12:51 +0200

    Cleanup

--- netlify/functions/getNews.js
+++ netlify/functions/getNews.js
@@ -1,5 +1,5 @@
-import { XMLParser } from "fast-xml-parser";
 import axios from 'axios';
+import { XMLParser } from "fast-xml-parser";
 
 export async function handler(event, context, callback) {
   const topics = ['jamstack', 'ssg', 'ssr', 'static site', 'serverless'];
@@ -7,18 +7,18 @@ export async function handler(event, context, callback) {
   const newsArticles = await Promise.allSettled(
     topics.map(
       async topic => await axios.get(`https://hnrss.org/newest?q=${topic}`)
-      .then(res => res.data)
+      .then(news => new XMLParser().parse(news.data, {
+          ignoreAttributes: false,
+          attributeNamePrefix: ''
+        }).rss?.channel?.item?.filter(item => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')) 
+        .map(({ title, link: url, pubDate: postDate}) => {return {title, url, postDate}})
+      )
       .catch(
         (error) => {
           callback(error);
         })
       )
-  ).then((values) => values.map(topic => new XMLParser().parse(topic.value, {
-      ignoreAttributes: false,
-      attributeNamePrefix: '',
-    }).rss?.channel?.item?.filter(item => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')) 
-    .map(({ title, link: url, pubDate: postDate}) => {return {title, url, postDate}})
-  ).flat().sort((a, b) => new Date(a.postDate) - new Date(b.postDate)).reverse())
+  ).then((values) => values.map(({value}) => value).flat().sort((a, b) => new Date(a.postDate) - new Date(b.postDate)).reverse())
       
   return {
     statusCode: newsArticles?.length ? 200 : 500,
