ff056649bfac7976fc16e5239f0ee245a355c798
Hal Wong <hal@pixelpusher.ca>
2023-07-12 14:36:13 +0200

    Update function

--- netlify/functions/getNews.js
+++ netlify/functions/getNews.js
@@ -2,22 +2,32 @@ import { XMLParser } from "fast-xml-parser";
 import fetch from 'node-fetch';
 
 export async function handler(event, context, callback) {
-  const response = await fetch('https://hnrss.org/newest?q=jamstack').then(res => res.text()).catch((error) => {
+  try {
+    const topics = ['jamstack', 'ssg']
+    const newsArticles = await Promise.allSettled(topics.map(async topic => await fetch(`https://hnrss.org/newest?q=${topic}`).then(res => res.text()))).then(async (values) => {
+      return values.map(topic => {
+        const news = new XMLParser().parse(topic.value, {
+          ignoreAttributes: false,
+          attributeNamePrefix: ''
+        });
+    
+        return news?.rss?.channel?.item?.filter(item => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')) 
+        .map(item => { 
+          return { title : item?.title, url : item?.link, postDate: item?.pubDate }
+        });  
+
+      }).flat().sort((a, b) => new Date(a.postDate) + new Date(b.postDate));
+      
+    })
+    
+    callback(null, {
+      statusCode: newsArticles?.length ? 200 : 500,
+      body: newsArticles?.length ? JSON.stringify({newsArticles}) : "An unknown error occurred"
+    }
+  )
+
+
+  } catch(error) {
     callback(error);
-  })
-
-  const news = new XMLParser().parse(response, {
-    ignoreAttributes: false,
-    attributeNamePrefix: '',
-  });
-
-  const newsArticles = news?.rss?.channel?.item?.filter(item => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')) 
-    .map(item => { 
-      return { title : item?.title, url : item?.link, postDate: item?.pubDate }
-    });
-
-  return {
-    statusCode: newsArticles?.length ? 200 : 500,
-    body: newsArticles?.length ? JSON.stringify({ newsArticles }) : undefined
-  };
+  }
 };
