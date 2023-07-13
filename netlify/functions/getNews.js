93c315618a9bc6e2d7e108967ce3c1c1e7166add
HalCodes <hal@thinkflo.com>
2023-07-13 08:30:48 +0200

    Cleanup and Formatting

--- netlify/functions/getNews.js
+++ netlify/functions/getNews.js
@@ -1,27 +1,45 @@
 import axios from 'axios';
-import { XMLParser } from "fast-xml-parser";
+import { XMLParser } from 'fast-xml-parser';
 
 export async function handler(event, context, callback) {
-  const topics = ['jamstack', 'ssg', 'ssr', 'static site', 'serverless'];
+	const topics = ['jamstack', 'ssg', 'ssr', 'static site', 'serverless'];
 
-  const newsArticles = await Promise.allSettled(
-    topics.map(
-      async topic => await axios.get(`https://hnrss.org/newest?q=${topic}`)
-      .then(news => new XMLParser().parse(news.data, {
-          ignoreAttributes: false,
-          attributeNamePrefix: ''
-        }).rss?.channel?.item?.filter(item => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')) 
-        .map(({ title, link: url, pubDate: postDate}) => {return {title, url, postDate}})
-      )
-      .catch(
-        (error) => {
-          callback(error);
-        })
-      )
-  ).then((values) => values.map(({value}) => value).flat().sort((a, b) => new Date(a.postDate) - new Date(b.postDate)).reverse())
-      
-  return {
-    statusCode: newsArticles?.length ? 200 : 500,
-    body: newsArticles?.length ? JSON.stringify({ newsArticles }) : undefined
-  };
-};
+	const newsArticles = await Promise.allSettled(
+		topics.map(
+			async (topic) =>
+				await axios
+					.get(`https://hnrss.org/newest?q=${topic}`)
+					.then((news) =>
+            news?.data && new XMLParser()
+							.parse(news?.data, {
+								ignoreAttributes: false,
+								attributeNamePrefix: ''
+							})
+							.rss?.channel?.item?.filter(
+								(item) => !item?.title.startsWith('Ask HN') && !item?.title.startsWith('Show HN')
+							)
+							.map(({ title, link: url, pubDate: postDate }) => {
+								return { title, url, postDate };
+							})
+					)
+					.catch(() => {
+						callback(null, {
+							statusCode: 500,
+							body: 'Internal Server Error'
+						});
+					})
+		)
+	).then((values) =>
+    [...new Set(values
+			.map(({ value }) => value)
+			.flat()
+			.sort((a, b) => new Date(a.postDate) - new Date(b.postDate))
+			.reverse()
+    )]
+	);
+
+	callback(null, {
+		statusCode: newsArticles?.length ? 200 : 500,
+		body: newsArticles?.length ? JSON.stringify({ newsArticles }) : 'Internal Server Error'
+	});
+}
