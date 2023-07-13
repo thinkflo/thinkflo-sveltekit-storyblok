ea502a6176d89cc3d4ef7f999699c5e472f8885a
HalCodes <hal@thinkflo.com>
2023-07-13 09:06:39 +0200

    Add CORS and propData

--- src/routes/[...slug]/+page.js
+++ src/routes/[...slug]/+page.js
@@ -1,20 +1,29 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, parent }) {
-  const { storyblokApi } = await parent();
-  let slug = params.slug;
-  let path = 'cdn/stories/';
-  let sbParams = {
-    version: "draft",
-    resolve_relations: ['Grid.Stories', 'Carousel.Stories']
-  };
+	const { storyblokApi } = await parent();
+	let slug = params.slug;
 
-  if (slug) {
-    path += slug;
-  } else {
-    path += 'home';
-  }
+	let path = 'cdn/stories/';
+	let sbParams = {
+		version: 'draft',
+		resolve_relations: ['Grid.Stories', 'Carousel.Stories']
+	};
+
+  path += slug ? slug : 'home'
+
   const dataStory = await storyblokApi.get(path, sbParams);
-  return {
-    story: dataStory.data.story
-  };
+
+	if (
+		dataStory.data?.story?.content?.Content_Blocks?.find(
+			(/** @type {{ component: string; }} */ block) => block.component === 'Trending_News'
+		)
+	) {
+		dataStory.data.story.content.Content_Blocks.find(
+			(block) => block.component === 'Trending_News'
+		)['data'] = await fetch('https://thinkflo.netlify.app/api/getNews').then((res) => res.json());
+	}
+
+	return {
+		story: dataStory.data.story
+	};
 }
