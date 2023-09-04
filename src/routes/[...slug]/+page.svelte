426f219429f51de80f19a1780e2cd005ac2e8490
Tony Spunei <tony.spunei@yahoo.com>
2023-09-04 11:24:22 +0200

    fix storyblok Live preview

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -1,8 +1,16 @@
 <script>
-	import { StoryblokComponent } from '@storyblok/svelte';
+	import { onMount } from 'svelte';
+	import { useStoryblokBridge, StoryblokComponent } from '@storyblok/svelte';
 	import Cookies_Monster from '$lib/Components/UI/Cookies_Monster.svelte';
 
 	export let data;
+
+	onMount(() => {
+		useStoryblokBridge(
+			data.story.id,
+			(newStory) => (data.story = newStory)
+		);
+});
 </script>
 
 <svelte:head>
@@ -11,9 +19,9 @@
 
 <main class="bg-gray-200">
 	{#if data.story}
-        {#key data.story.content._uid}
-		    <StoryblokComponent blok={data.story.content} />
-        {/key}
+		{#key data.story.content._uid}
+			<StoryblokComponent blok={data.story.content} />
+		{/key}
 	{/if}
 	<Cookies_Monster />
 </main>
