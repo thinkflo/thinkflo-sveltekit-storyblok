ca4967501d3e3bd1cec3293b2199d0eb3e0d483b
Tony Spunei <tony.spunei@yahoo.com>
2023-09-08 14:16:05 +0200

    render Eras coming from Storyblok

--- src/lib/Components/Content_Blocks/Timeline.svelte
+++ src/lib/Components/Content_Blocks/Timeline.svelte
@@ -6,82 +6,82 @@
 
   console.log(blok)
   let data = {
-      "title": {
+    "title": {
+      "text": {
+        "headline": blok?.Heading || "",
+        "text": blok?.Blurb || ""
+      },
+      "media": {
+        "url": blok?.Media?.[0]?.Image?.filename,
+        "caption": blok?.Media?.[0]?.Caption,
+        "credit": blok?.Media?.[0]?.Credit
+      }
+    },
+    "events": [
+      {
+        "start_date": {
+          "year": 2020,
+          "month": 1,
+          "day": 1
+        },
+        "end_date": {
+          "year": 2020,
+          "month": 12,
+          "day": 31
+        },
         "text": {
-          "headline": blok?.Heading || "",
-          "text": blok?.Blurb || ""
+          "headline": "Event 1",
+          "text": "Description for event 1."
         },
         "media": {
-          "url": blok?.Media?.[0]?.Image?.filename,
-          "caption": blok?.Media?.[0]?.Caption,
-          "credit": blok?.Media?.[0]?.Credit
-        }
-      },
-      "events": [
-        {
-          "start_date": {
-            "year": 2020,
-            "month": 1,
-            "day": 1
-          },
-          "end_date": {
-            "year": 2020,
-            "month": 12,
-            "day": 31
-          },
-          "text": {
-            "headline": "Event 1",
-            "text": "Description for event 1."
-          },
-          "media": {
-            "url": "https://example.com/image-for-event-1.jpg",
-            "caption": "Caption for event 1",
-            "credit": "Image Credit for event 1"
-          },
-          "group": "Group A",
-          "display_date": "2020",
-          "background": {
-            "color": "#ff0000"
-          },
-          "unique_id": "event1"
+          "url": "https://example.com/image-for-event-1.jpg",
+          "caption": "Caption for event 1",
+          "credit": "Image Credit for event 1"
         },
-        {
-          "start_date": {
-            "year": 2021
-          },
-          "text": {
-            "headline": "Event 2",
-            "text": "Description for event 2."
-          },
-          "media": {
-            "url": "https://example.com/image-for-event-2.jpg",
-            "caption": "Caption for event 2",
-            "credit": "Image Credit for event 2"
-          },
-          "group": "Group B",
-          "display_date": "2021",
-          "background": {
-            "color": "#00ff00"
-          },
-          "unique_id": "event2"
-        }
-      ],
-      "eras": [
-        {
-          "start_date": {
-            "year": 2020
-          },
-          "end_date": {
-            "year": 2021
-          },
-          "text": {
-            "headline": "Era 1",
-            "text": "Description for Era 1"
-          }
-        }
-      ]
+        "group": "Group A",
+        "display_date": "2020",
+        "background": {
+          "color": "#ff0000"
+        },
+        "unique_id": "event1"
+      },
+      {
+        "start_date": {
+          "year": 2021
+        },
+        "text": {
+          "headline": "Event 2",
+          "text": "Description for event 2."
+        },
+        "media": {
+          "url": "https://example.com/image-for-event-2.jpg",
+          "caption": "Caption for event 2",
+          "credit": "Image Credit for event 2"
+        },
+        "group": "Group B",
+        "display_date": "2021",
+        "background": {
+          "color": "#00ff00"
+        },
+        "unique_id": "event2"
+      }
+    ],
+    "eras": []
+  }
+
+  data.eras = blok.Eras.map((era) => ({ 
+    "start_date": {
+      "year": era.Start_Date
+    },
+    "end_date": {
+      "year": era.End_Date
+    },
+    "text": {
+      "headline": era.Heading,
+      "text": era.Blurb
     }
-  let timeline_json = JSON.stringify(data);
+  }));
+
   
   onMount(async () => {
     const { Timeline } = await import('@knight-lab/timelinejs');
