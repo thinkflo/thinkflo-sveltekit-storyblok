1558fd67df607b4aa07d7f1ce487209b5b69be6f
Tony Spunei <tony.spunei@yahoo.com>
2023-09-11 14:56:27 +0200

    adjust footer bg and text color

--- src/lib/Components/Global_Components/Footer.svelte
+++ src/lib/Components/Global_Components/Footer.svelte
@@ -6,11 +6,11 @@
 	const currentYear = new Date().getFullYear();
 </script>
 
-<footer class="w-full py-12 md:py-24 md:pb-5 bg-slate-900 text-white">
+<footer class="w-full py-12 md:py-24 md:pb-5 text-black">
 	<Constrained_Width
 		className="container h-full mx-auto flex flex-col space-y-10 items-center justify-between"
 	>
-		<a href="/"><img src="/logo-white.svg" alt="Thinkflo Logo White" class="h-10 mx-auto" /></a>
+		<a href="/"><img src="/logo.svg" alt="Thinkflo Logo White" class="h-10 mx-auto" /></a>
 		<nav class="flex justify-center space-x-12" aria-label="Footer">
 			{#if blok?.Nav_Menu?.length}
 				{#each blok.Nav_Menu as Menu_Item}
@@ -18,15 +18,15 @@
 						<a
 							href={`/${Menu_Item?.Link?.cached_url}`}
 							data-sveltekit-preload-data="hover"
-							class="text-sm leading-6 text-white hover:text-gray-300"
+							class="text-sm leading-6 hover:text-gray-300"
 						>
 							{Menu_Item?.Name}
-						</a>
+						</a> 
 					</div>
 				{/each}
 			{/if}
 		</nav>
-		<p class="text-center text-xs leading-5 text-white">
+		<p class="text-center text-xs leading-5">
 			&copy; {currentYear} Thinkflo All rights reserved.
 		</p>
 	</Constrained_Width>
